e068baf557a7b536bb664bafd834f02d9d58a590
ratnahalder <ratnacse06@gmail.com>
2021-01-10 01:38:53 +0000

    gazebo world making

---
+++ Project1/script/welcome_message.cpp
@@ -0,0 +1,17 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginWelcomeMessage : public WorldPlugin
+  {
+    public: WorldPluginWelcomeMessage() : WorldPlugin()
+            {
+              printf("Welcome to Udacity's World!\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginWelcomeMessage)
+}
