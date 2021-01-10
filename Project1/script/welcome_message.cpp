9df266da4d9550f382af5ad395c4f855c51882ff
ratnahalder <ratnacse06@gmail.com>
2021-01-10 19:25:17 +0000

    HumanoidRobot model is not perfect

--- Project1/script/welcome_message.cpp
+++ Project1/script/welcome_message.cpp
@@ -6,7 +6,7 @@ namespace gazebo
   {
     public: WorldPluginWelcomeMessage() : WorldPlugin()
             {
-              printf("Welcome to Udacity's World!\n");
+              printf("Welcome to Ratna's World!\n");
             }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
